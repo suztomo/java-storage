0df854e9320b079431acd039093be8355a66dd52
Frank Natividad <franknatividad@google.com>
2020-04-06 23:48:43 -0700

    prototype grpc support

--- google-cloud-storage/src/main/java/com/google/cloud/storage/StorageOptions.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/StorageOptions.java
@@ -16,16 +16,23 @@
 
 package com.google.cloud.storage;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.NoCredentials;
 import com.google.cloud.ServiceDefaults;
 import com.google.cloud.ServiceOptions;
 import com.google.cloud.ServiceRpc;
 import com.google.cloud.TransportOptions;
+import com.google.cloud.google.storage.v1.StorageSettings;
+import com.google.cloud.grpc.GrpcTransportOptions;
 import com.google.cloud.http.HttpTransportOptions;
 import com.google.cloud.storage.spi.StorageRpcFactory;
+import com.google.cloud.storage.spi.v1.GrpcStorageRpc;
 import com.google.cloud.storage.spi.v1.HttpStorageRpc;
 import com.google.cloud.storage.spi.v1.StorageRpc;
 import com.google.common.collect.ImmutableSet;
+import java.io.IOException;
 import java.util.Set;
 
 public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
@@ -34,7 +41,11 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
   private static final String API_SHORT_NAME = "Storage";
   private static final String GCS_SCOPE = "https://www.googleapis.com/auth/devstorage.full_control";
   private static final Set<String> SCOPES = ImmutableSet.of(GCS_SCOPE);
-  private static final String DEFAULT_HOST = "https://storage.googleapis.com";
+  private static final String DEFAULT_HTTP_HOST = "https://storage.googleapis.com";
+  private static final String DEFAULT_GRPC_HOST = "storage.googleapis.com:443";
+  private final boolean grpcEnabled;
+  private final TransportChannelProvider channelProvider;
+  private final CredentialsProvider credentialsProvider;
 
   public static class DefaultStorageFactory implements StorageFactory {
 
@@ -52,35 +63,98 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
 
     @Override
     public ServiceRpc create(StorageOptions options) {
-      return new HttpStorageRpc(options);
+      if (options.getGrpcEnabled()) {
+        return new GrpcStorageRpc(options);
+      } else {
+        return new HttpStorageRpc(options);
+      }
     }
   }
 
   public static class Builder extends ServiceOptions.Builder<Storage, StorageOptions, Builder> {
 
+    private boolean grpcEnabled = false;
+    private TransportChannelProvider channelProvider = null;
+    private CredentialsProvider credentialsProvider = null;
+
     private Builder() {}
 
     private Builder(StorageOptions options) {
       super(options);
+      this.grpcEnabled = options.grpcEnabled;
+      this.channelProvider = options.channelProvider;
+      this.credentialsProvider = options.credentialsProvider;
     }
 
     @Override
     public Builder setTransportOptions(TransportOptions transportOptions) {
-      if (!(transportOptions instanceof HttpTransportOptions)) {
-        throw new IllegalArgumentException(
-            "Only http transport is allowed for " + API_SHORT_NAME + ".");
-      }
       return super.setTransportOptions(transportOptions);
+    }
+
+    /**
+     * Sets the {@link TransportChannelProvider} to use with this Firestore client.
+     *
+     * @param channelProvider A InstantiatingGrpcChannelProvider object that defines the transport
+     *     provider for this client.
+     */
+    public Builder setChannelProvider(TransportChannelProvider channelProvider) {
+      if (!(channelProvider instanceof InstantiatingGrpcChannelProvider)) {
+        throw new IllegalArgumentException(
+            "Only GRPC channels are allowed for " + API_SHORT_NAME + ".");
+      }
+      this.channelProvider = channelProvider;
+      return this;
+    }
+
+    /**
+     * Sets the {@link CredentialsProvider} to use with this Firestore client.
+     *
+     * @param credentialsProvider A CredentialsProvider object that defines the credential provider
+     *     for this client.
+     */
+    public Builder setCredentialsProvider(CredentialsProvider credentialsProvider) {
+      this.credentialsProvider = credentialsProvider;
+      return this;
+    }
+
+    public Builder setGrpcEnabled(boolean enabled) {
+      this.grpcEnabled = enabled;
+      return this;
     }
 
     @Override
     public StorageOptions build() {
+      if (this.credentials == null && this.credentialsProvider != null) {
+        try {
+          this.setCredentials(credentialsProvider.getCredentials());
+        } catch (IOException e) {
+          throw new RuntimeException("Failed to obtain credentials", e);
+        }
+      }
       return new StorageOptions(this);
     }
   }
 
   private StorageOptions(Builder builder) {
     super(StorageFactory.class, StorageRpcFactory.class, builder, new StorageDefaults());
+
+    this.grpcEnabled = builder.grpcEnabled;
+
+    if (this.grpcEnabled) {
+      this.channelProvider =
+          builder.channelProvider != null
+              ? builder.channelProvider
+              : GrpcTransportOptions.setUpChannelProvider(
+                  StorageSettings.defaultGrpcTransportProviderBuilder(), this);
+
+      this.credentialsProvider =
+          builder.credentialsProvider != null
+              ? builder.credentialsProvider
+              : GrpcTransportOptions.setUpCredentialsProvider(this);
+    } else {
+      this.channelProvider = null;
+      this.credentialsProvider = null;
+    }
   }
 
   private static class StorageDefaults implements ServiceDefaults<Storage, StorageOptions> {
@@ -105,6 +179,10 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
     return HttpTransportOptions.newBuilder().build();
   }
 
+  public static GrpcTransportOptions getDefaultGrpcTransportOptions() {
+    return getDefaultTransportOptionsBuilder().build();
+  }
+
   // Project ID is only required for creating buckets, so we don't require it for creating the
   // service.
   @Override
@@ -121,9 +199,33 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
     return (StorageRpc) getRpc();
   }
 
+  public static GrpcTransportOptions.Builder getDefaultTransportOptionsBuilder() {
+    return GrpcTransportOptions.newBuilder();
+  }
+
+  public CredentialsProvider getCredentialsProvider() {
+    return credentialsProvider;
+  }
+
+  public TransportChannelProvider getTransportChannelProvider() {
+    return channelProvider;
+  }
+
+  public boolean getGrpcEnabled() {
+    return grpcEnabled;
+  }
+
   /** Returns a default {@code StorageOptions} instance. */
   public static StorageOptions getDefaultInstance() {
     return newBuilder().build();
+  }
+
+  public static StorageOptions getDefaultGrpcInstance() {
+    return newBuilder()
+        .setTransportOptions(getDefaultGrpcTransportOptions())
+        .setGrpcEnabled(true)
+        .setHost(DEFAULT_GRPC_HOST)
+        .build();
   }
 
   /** Returns a unauthenticated {@code StorageOptions} instance. */
@@ -134,7 +236,7 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
   @SuppressWarnings("unchecked")
   @Override
   public Builder toBuilder() {
-    return new Builder(this).setHost(DEFAULT_HOST);
+    return new Builder(this).setHost(DEFAULT_HTTP_HOST);
   }
 
   @Override
@@ -148,6 +250,6 @@ public class StorageOptions extends ServiceOptions<Storage, StorageOptions> {
   }
 
   public static Builder newBuilder() {
-    return new Builder().setHost(DEFAULT_HOST);
+    return new Builder().setHost(DEFAULT_HTTP_HOST);
   }
 }
